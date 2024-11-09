f0d96941fa0df35200b56a66837a71b5594e9e2a
Luis Llanes <luisllaboj@gmail.com>
2024-11-09 14:05:23 -0700

    chore: dark mode based on system pref

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import { Toaster } from "sonner";
 import { WelcomeToast } from "./welcome-toast";
 import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: {
@@ -28,103 +29,110 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
+    <html lang="en" className="h-full" suppressHydrationWarning>
       <body
         className={`${GeistSans.variable} ${GeistMono.variable} flex flex-col antialiased`}
       >
-        <div>
-          <header className="z-10 flex flex-grow items-center justify-between gap-4 border-b-2 border-accent2 bg-background p-2 pb-[4px] pt-2 sm:flex-row sm:p-4 sm:pb-[4px] sm:pt-0">
-            <div className="flex flex-grow flex-col">
-              <div className="absolute right-2 top-2 flex justify-end pt-2 font-sans text-sm hover:underline sm:relative sm:right-0 sm:top-0">
-                <Suspense
-                  fallback={
-                    <button className="flex flex-row items-center gap-1">
-                      <div className="h-[20px]" />
-                      <svg viewBox="0 0 10 6" className="h-[6px] w-[10px]">
-                        <polygon points="0,0 5,6 10,0"></polygon>
-                      </svg>
-                    </button>
-                  }
-                >
-                  <AuthServer />
-                </Suspense>
-              </div>
-              <div className="flex w-full flex-col items-start justify-center gap-2 sm:w-auto sm:flex-row sm:items-center">
-                <Link
-                  prefetch={true}
-                  href="/"
-                  className="text-4xl font-bold text-accent1"
-                >
-                  NextFaster
-                </Link>
-                <div className="items flex w-full flex-row items-center justify-between gap-4">
-                  <div className="mx-0 flex-grow sm:mx-auto sm:flex-grow-0">
-                    <SearchDropdownComponent />
-                  </div>
-                  <div className="flex flex-row justify-between space-x-4">
-                    <div className="relative">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div>
+            <header className="z-10 flex flex-grow items-center justify-between gap-4 border-b-2 border-accent2 bg-background p-2 pb-[4px] pt-2 sm:flex-row sm:p-4 sm:pb-[4px] sm:pt-0">
+              <div className="flex flex-grow flex-col">
+                <div className="absolute right-2 top-2 flex justify-end pt-2 font-sans text-sm hover:underline sm:relative sm:right-0 sm:top-0">
+                  <Suspense
+                    fallback={
+                      <button className="flex flex-row items-center gap-1">
+                        <div className="h-[20px]" />
+                        <svg viewBox="0 0 10 6" className="h-[6px] w-[10px]">
+                          <polygon points="0,0 5,6 10,0"></polygon>
+                        </svg>
+                      </button>
+                    }
+                  >
+                    <AuthServer />
+                  </Suspense>
+                </div>
+                <div className="flex w-full flex-col items-start justify-center gap-2 sm:w-auto sm:flex-row sm:items-center">
+                  <Link
+                    prefetch={true}
+                    href="/"
+                    className="text-4xl font-bold text-accent1"
+                  >
+                    NextFaster
+                  </Link>
+                  <div className="items flex w-full flex-row items-center justify-between gap-4">
+                    <div className="mx-0 flex-grow sm:mx-auto sm:flex-grow-0">
+                      <SearchDropdownComponent />
+                    </div>
+                    <div className="flex flex-row justify-between space-x-4">
+                      <div className="relative">
+                        <Link
+                          prefetch={true}
+                          href="/order"
+                          className="text-lg text-accent1 hover:underline"
+                        >
+                          ORDER
+                        </Link>
+                        <Suspense>
+                          <Cart />
+                        </Suspense>
+                      </div>
                       <Link
                         prefetch={true}
-                        href="/order"
-                        className="text-lg text-accent1 hover:underline"
+                        href="/order-history"
+                        className="hidden text-lg text-accent1 hover:underline md:block"
                       >
-                        ORDER
+                        ORDER HISTORY
                       </Link>
-                      <Suspense>
-                        <Cart />
-                      </Suspense>
+                      <Link
+                        prefetch={true}
+                        href="/order-history"
+                        aria-label="Order History"
+                        className="block text-lg text-accent1 hover:underline md:hidden"
+                      >
+                        <MenuIcon />
+                      </Link>
                     </div>
-                    <Link
-                      prefetch={true}
-                      href="/order-history"
-                      className="hidden text-lg text-accent1 hover:underline md:block"
-                    >
-                      ORDER HISTORY
-                    </Link>
-                    <Link
-                      prefetch={true}
-                      href="/order-history"
-                      aria-label="Order History"
-                      className="block text-lg text-accent1 hover:underline md:hidden"
-                    >
-                      <MenuIcon />
-                    </Link>
                   </div>
                 </div>
               </div>
+            </header>
+            {children}
+          </div>
+          <footer className="flex h-[8px] flex-col items-center justify-between space-y-2 border-t border-gray-400 bg-background px-4 font-sans text-[11px] sm:h-6 sm:flex-row sm:space-y-0">
+            <div className="flex flex-wrap justify-center space-x-2 pt-2 sm:justify-start">
+              <span className="hover:bg-accent2 hover:underline">Home</span>
+              <span>|</span>
+              <span className="hover:bg-accent2 hover:underline">FAQ</span>
+              <span>|</span>
+              <span className="hover:bg-accent2 hover:underline">Returns</span>
+              <span>|</span>
+              <span className="hover:bg-accent2 hover:underline">Careers</span>
+              <span>|</span>
+              <span className="hover:bg-accent2 hover:underline">Contact</span>
             </div>
-          </header>
-          {children}
-        </div>
-        <footer className="flex h-[8px] flex-col items-center justify-between space-y-2 border-t border-gray-400 bg-background px-4 font-sans text-[11px] sm:h-6 sm:flex-row sm:space-y-0">
-          <div className="flex flex-wrap justify-center space-x-2 pt-2 sm:justify-start">
-            <span className="hover:bg-accent2 hover:underline">Home</span>
-            <span>|</span>
-            <span className="hover:bg-accent2 hover:underline">FAQ</span>
-            <span>|</span>
-            <span className="hover:bg-accent2 hover:underline">Returns</span>
-            <span>|</span>
-            <span className="hover:bg-accent2 hover:underline">Careers</span>
-            <span>|</span>
-            <span className="hover:bg-accent2 hover:underline">Contact</span>
-          </div>
-          <div className="text-center sm:text-right">
-            By using this website, you agree to check out the{" "}
-            <Link
-              href="https://github.com/ethanniser/NextFaster"
-              className="font-bold text-accent1 hover:underline"
-              target="_blank"
-            >
-              Source Code
-            </Link>
-          </div>
-        </footer>
-        {/* does putting this in suspense do anything? */}
-        <Suspense fallback={null}>
-          <Toaster closeButton />
-          <WelcomeToast />
-        </Suspense>
-        <Analytics scriptSrc="/insights/events.js" endpoint="/hfi/events" />
+            <div className="text-center sm:text-right">
+              By using this website, you agree to check out the{" "}
+              <Link
+                href="https://github.com/ethanniser/NextFaster"
+                className="font-bold text-accent1 hover:underline"
+                target="_blank"
+              >
+                Source Code
+              </Link>
+            </div>
+          </footer>
+          {/* does putting this in suspense do anything? */}
+          <Suspense fallback={null}>
+            <Toaster closeButton />
+            <WelcomeToast />
+          </Suspense>
+          <Analytics scriptSrc="/insights/events.js" endpoint="/hfi/events" />
+        </ThemeProvider>
       </body>
     </html>
   );
